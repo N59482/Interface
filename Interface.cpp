9a4d4f929c6b151629aa138c43fc57e8336821a1
N59482 <iduginmail@ya.ru>
2022-01-12 17:23:05 +0300

    Набросок реализации интерфейсов в C++

--- Interface.cpp
+++ Interface.cpp
@@ -3,42 +3,84 @@
 
 using namespace std;
 
-// class IUse
-// 	{
-// 		virtual void use(Object obj) = 0;
-// 	};
-
-class Object
+class IUse
 	{
+		virtual void use() = 0;
+		virtual void eat() = 0;
+		virtual void kick() = 0;
+	};
+
+class Object : public IUse
+	{
+	    public:
 		string name;
-		public:
-		void print()
+		void use() override
 			{
 				cout<<this->name<<" used\n";
 			};
-		Object() :  name
+		void eat() override
 			{
-				this->name = "no_name";
+				cout<<this->name<<" is eaten\n";
+			};
+		void kick() override
+			{
+				cout<<this->name<<" is kicked\n";
 			};
 		Object(string name)
 			{
 				this->name = name;
 			};
+	    Object() :  Object("something")
+	        {};
 	};
 
-class User //: public IUse
+class Car : public Object
+	{
+		void use() override
+			{
+				cout<<this->name<<" started\n";
+			};
+		void eat() override
+			{
+				cout<<"Eating a "<<this->name<<" isn't a good idea...\n";
+			};
+		void kick() override
+			{
+				cout<<"The "<<this->name<<" alarm went off\n";	
+			};
+	};
+
+class User
 	{
 	    public:
-		void use(Object obj) //override
+		void use(Object obj) 
 			{
-				obj.print();
+				obj.use();
 			};
+		void eat(Object obj)
+			{
+				obj.eat();
+			};
+		void kick(Object obj)
+			{
+				obj.kick();
+			};
+	};
+
+void Mark_test(const Object &o)
+	{
+		User mark;
+		mark.use(o);
+		mark.eat(o);
+		mark.kick(o);
 	};
 
 int main()
 	{
-		Object pen("pen");
-		User mark;
-		mark.use(pen);
+		Object thing;
+		Mark_test(thing);
+		Car audi;
+		Mark_test(audi);
+		
 		return 0;
 	};
